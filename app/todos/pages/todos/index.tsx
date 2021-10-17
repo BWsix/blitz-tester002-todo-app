c1d5d6a9132aa18e09e7605035a390dbd5453d4e
BWsix <itisbw62@gmail.com>
2021-10-17 21:05:38 +0800

    refactor(todo): update ui
    using mui x data grid
    https://mui.com/components/data-grid/

--- app/todos/pages/todos/index.tsx
+++ app/todos/pages/todos/index.tsx
@@ -1,61 +1,7 @@
 import Layout from "app/core/layouts/Layout"
-import { CreateTodoForm } from "app/todos/components/CreateTodoForm"
-import deleteTodos from "app/todos/mutations/deleteTodos"
-import updateTodo from "app/todos/mutations/updateTodo"
-import getTodos from "app/todos/queries/getTodos"
-import { BlitzPage, Head, invalidateQuery, Link, Routes, useMutation, useQuery } from "blitz"
+import { BlitzPage, Head } from "blitz"
 import { Suspense } from "react"
-
-export const TodosList = () => {
-  const [todos] = useQuery(getTodos, {})
-  const [updateTodoMutation] = useMutation(updateTodo, {
-    onSuccess: async () => await invalidateQuery(getTodos),
-  })
-  const [deleteCheckedTodoMutation] = useMutation(deleteTodos, {
-    onSuccess: async () => await invalidateQuery(getTodos),
-  })
-
-  const todoContainsChecked = todos.map((todo) => todo.checked).includes(true)
-
-  return (
-    <>
-      {todos.length ? "todos :" : "(no todo)"}
-
-      <div>
-        {todos.map((todo) => (
-          <div key={todo.id}>
-            <input
-              type="checkbox"
-              checked={todo.checked}
-              readOnly
-              onClick={() => {
-                updateTodoMutation({ id: todo.id, checked: !todo.checked })
-              }}
-            />
-            <Link href={Routes.ShowTodoPage({ todoId: todo.id })}>
-              <a>{todo.title}</a>
-            </Link>
-          </div>
-        ))}
-        ------------------
-        <div>
-          <button
-            disabled={!todoContainsChecked}
-            onClick={async () => {
-              await deleteCheckedTodoMutation()
-            }}
-          >
-            delete checked
-          </button>
-        </div>
-        ------------------
-        <div>
-          <CreateTodoForm />
-        </div>
-      </div>
-    </>
-  )
-}
+import { TodosList } from "../../components/TodosList"
 
 const TodosPage: BlitzPage = () => {
   return (
